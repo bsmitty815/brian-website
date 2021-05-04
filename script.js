0857352f26708e7e12d0fb437e0b88784c6215e3
Brian Smith <brian.bubbam@gmail.com>
2021-05-04 17:57:37 -0500

    added some images

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-
-document.querySelector('body').appendChild(h2).style.textAlign = "center";
 
 
-const ballGame = document.createElementID('BallGame')
+var imgObj = null;
+function init(){
+    imgObj = document.getElementById('bruno');
+    imgObj.style.position= 'relative';
+    imgObj.style.left = '0px';
+ }    
+ function moveRight(){
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+
+
+ }
+ window.onload =init;
+
+
+ let mylie = document.getElementById("mylie");
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+mylie.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.imgObj = "bruno";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);  
