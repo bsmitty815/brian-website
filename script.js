fc75fa07b7f283f8043e1caf5ae180b59fc1f59d
Brian Smith <brian.bubbam@gmail.com>
2021-05-04 10:54:32 -0500

    updated layout for html, css, javascript, and added image

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 
-document.querySelector('body').appendChild(h2); 
+document.querySelector('body').appendChild(h2).style.textAlign = "center";
+
+
+const ballGame = document.createElementID('BallGame')
